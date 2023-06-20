dd4f26dc177b2e48e14f4c3c9016dad683078573
rmeroy0oos <136660217+rmeroy0oos@users.noreply.github.com>
2023-06-20 08:09:24 +0800

    Dejee Portfolio Wordpress

---
+++ reyjudeleroywpdejee/footer.php
@@ -0,0 +1,29 @@
+<footer>
+    <div class="container justi d-flex align-items-center">
+            <div class="row">
+                <div class="col-sm-4">
+                    <a class="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover" href="#">
+                        Home
+                    </a>
+                </div>
+                <div class="col-sm-4">
+                    <a class="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover" href="#">
+                        About
+                    </a>
+                </div>
+                <div class="col-sm-4">
+                    <a class="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover" href="#">
+                        Contact Us
+                    </a>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <i class="bi bi-facebook ms-5 p-2" style="font-size: 2rem;"></i>
+                    <i class="bi bi-twitter ms-5 p-2" style="font-size: 2rem;"></i>
+                    <i class="bi bi-instagram ms-5 auto p-2" style="font-size: 2rem;"></i>
+                    <i class="bi bi-youtube ms-5 p-2" style="font-size: 2rem;"></i>
+                </div>
+            </div>
+    </div>
+</footer>
---
+++ reyjudeleroywpdejee/header.php
@@ -0,0 +1,20 @@
+<header>
+    <nav class="navbar fixed-top navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand display-2 fs-1 offset-1" href="#">Portfolio</a>
+            <div class="collapse navbar-collapse offset-5" id="navbarNav">
+                <ul class="navbar-nav  ">
+                    <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">Contact Us</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
